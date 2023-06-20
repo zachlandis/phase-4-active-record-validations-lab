4127a29893ba0523978e7bf2bffc291c12a15aaf
Zach <zachlandis91@gmail.com>
2023-06-20 14:36:03 -0600

    commit tree c39cd2761a3a1ca065355cad83c81e4e98286bfb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    validate :clickbait
+
+    def clickbait
+        if title == "True Facts"
+            errors.add(:title, "must be clickbait")
+        end
+    end
 end
